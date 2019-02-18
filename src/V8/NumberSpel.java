b3e7cb86a4d25efbe89ce890d71a7e6fbb853013
Thekolllol90 <neosvarre@hotmail.se>
2019-02-18 10:10:18 +0100

    skapa om skrev

---
+++ src/V8/NumberSpel.java
@@ -0,0 +1,20 @@
+package V8;
+import java.util.Scanner;
+import java.util.Random;
+
+public class NumberSpel {
+	public static void main(String[] args) {
+	
+		Random random = new Random(); //genererar ett random tal
+		int tal = random.nextInt(101); // hämtar ett number mellan 0-100
+	}
+	
+	
+	public int Spel(int tal) {
+		Scanner scanner = new Scanner(System.in);
+		
+		return 0;
+	}
+}
+
+

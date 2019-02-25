adb0b9993fc3d2dd4d17f9e2d6c0bc35a875ea14
Thekolllol90 <neosvarre@hotmail.se>
2019-02-25 10:07:32 +0100

    fixat så svårhetsgrader funkar

--- src/V8/NumberSpel.java
+++ src/V8/NumberSpel.java
@@ -1,48 +1,66 @@
 package V8;
+
 import java.util.Scanner; //importerar scanner
 import java.util.Random;// importerar random
 
 public class NumberSpel {
 	public static void main(String[] args) {
-	
+
 		boolean again = true;
 		Scanner sc = new Scanner(System.in);
-		
-	while(again == true){
 		Random random = new Random(); // genererar ett random tal
-		int tal = random.nextInt(101); // hämtar ett number mellan 0-100
-		System.out.println("Im thinking of a number betwen 0 and 100");
-		
-		Spel(tal); // hämtar methoden 
-		
-		System.out.println("Do you want to play again?");
-		System.out.println();
-		System.out.println("Y" + "    " + "N");
-		String ag = sc.next();
-		if(ag.toLowerCase().equals("y")) { // gör alla siffror till lowercase och kollar om man skriver Y
-			again = true;
-		}else if(ag.toLowerCase().equals("n")) { // gör alla siffror till lowercase och kollar om man skriver N
-			again = false; // stoppar loopen som kör om spelet
+		// String dif = "0";
+		while (again == true) {
+
+			System.out.println("choose a difficulty");
+			System.out.println();
+			System.out.println("1.easy   2.medium   3.hard  4.imposible");
+			// dif = sc.nextLine();
+			int dif = sc.nextInt();
+
+			int tal = 0;
+			int val = 0;
+
+			if (dif == 1) { //ger olika antal möjliga tal beroende på vilken svårhetsgrad man väljer
+				val = 101;
+			} else if (dif == 2) {
+				val = 151;
+			} else if (dif == 3) {
+				val = 201;
+			}
+			System.out.println("Im thinking of a number betwen 0 and " + (val - 1));
+			tal = random.nextInt(val);
+			Spel(tal); // hämtar methoden
+
+			System.out.println("Do you want to play again?");
+			System.out.println();
+			System.out.println("Y" + "    " + "N");
+			String ag = sc.next();
+			if (ag.toLowerCase().equals("y")) { // gör alla siffror till lowercase och kollar om man skriver Y
+				again = true;
+			} else if (ag.toLowerCase().equals("n")) { // gör alla siffror till lowercase och kollar om man skriver N
+				again = false; // stoppar loopen som kör om spelet
+				System.out.println("Okay, Goodbye");
+			}
 		}
-		}
+		sc.close();
 	}
-	
-	
+
 	public static int Spel(int tal) {
 		Scanner scanner = new Scanner(System.in); // skapar en ny scanner
-		Boolean spel = true; 
+		Boolean spel = true;
 		int y = 0;
-		
-		while(spel=true) { // skapar en while loop
-			
+
+		while (spel == true) { // skapar en while loop
+
 			int x = scanner.nextInt();
 			y++;
-			
-			if(x < tal) { // om talet man skriver är mindre än talet skriver den högre
+
+			if (x < tal) { // om talet man skriver är mindre än talet skriver den högre
 				System.out.println("higher");
-			} else if(x > tal) { // om talet man skriver är störe än talet skriver den läggre
+			} else if (x > tal) { // om talet man skriver är störe än talet skriver den läggre
 				System.out.println("Lower");
-			} else if(x == tal) { // om man gissar rätt tal skriver den att man vunnit
+			} else if (x == tal) { // om man gissar rätt tal skriver den att man vunnit
 				System.out.println();
 				System.out.println();
 				System.out.println("You Won");
@@ -50,9 +68,7 @@ public class NumberSpel {
 				break; // går ur loopen
 			}
 		}
-		
+
 		return 0;
 	}
 }
-
-

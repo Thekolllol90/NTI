675017d1d2caba6ec9cea4c8079ca700af8ceb5c
Thekolllol90 <neosvarre@hotmail.se>
2019-02-25 10:54:56 +0100

    fungerande poänglista
    med hjälp av patrik

--- src/V8/NumberSpel.java
+++ src/V8/NumberSpel.java
@@ -1,17 +1,75 @@
 package V8;
 
-import java.util.Scanner; //importerar scanner
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Random;// importerar random
+import java.util.Scanner; //importerar scanner
 
 public class NumberSpel {
+
+	public static void sparaArray(int[] array) {
+		FileWriter fileWriter;
+		try {
+			fileWriter = new FileWriter("array.txt");
+			for (int i = 0; i < array.length; i++) {
+				if (i == array.length - 1)
+					fileWriter.write(String.format("%d", array[i]));
+				else
+					fileWriter.write(array[i] + ",");
+			}
+
+			fileWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static int[] hämtaArray() {
+		FileReader fileReader;
+		try {
+			fileReader = new FileReader("array.txt");
+            BufferedReader reader = new BufferedReader(fileReader);
+
+            String line = reader.readLine();
+            String[] values = line.split(",");
+            
+            int[] result = new int[values.length];
+            for(int i = 0; i < values.length; i++) {
+            	result[i] = Integer.parseInt(values[i]);
+            }
+            
+            reader.close();
+            fileReader.close();
+            
+            return result;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
 	public static void main(String[] args) {
 
 		boolean again = true;
 		Scanner sc = new Scanner(System.in);
 		Random random = new Random(); // genererar ett random tal
-		int intarray[] = {		// skaapr en array som poäng lista
-				99, 88, 33, 44, 55
-		};
+
+		int intarray[] = { // skaapr en array som poäng lista
+				99, 88, 33, 44, 55 };
+
+		intarray = hämtaArray();
+		
+		for (int i = 0; i < intarray.length; i++) { // skriver ut arrayen som en poång lista
+			System.out.println(intarray[i]);
+		}
+
+
 		while (again == true) {
 
 			System.out.println("choose a difficulty");
@@ -22,8 +80,8 @@ public class NumberSpel {
 			int tal = 0;
 			int val = 0;
 
-			if (dif == 1) { //ger olika antal möjliga tal beroende på vilken svårhetsgrad man väljer
-				val = 101;
+			if (dif == 1) { // ger olika antal möjliga tal beroende på vilken svårhetsgrad man väljer
+				val = 4;
 			} else if (dif == 2) {
 				val = 151;
 			} else if (dif == 3) {
@@ -32,11 +90,16 @@ public class NumberSpel {
 			System.out.println("Im thinking of a number betwen 0 and " + (val - 1));
 			tal = random.nextInt(val);
 			int rep = Spel(tal); // hämtar methoden
-			
-			
-			intarray[intarray.length - 1]
-			
-			bubbleSort(intarray);
+
+			bubbleSort(intarray); // hämtar bubble sort som sorterar arrayen
+
+			if (rep < intarray[intarray.length - 1]) {
+				intarray[intarray.length - 1] = rep;
+			}
+
+			bubbleSort(intarray); // hämtar bubble sort som sorterar arrayen
+
+			sparaArray(intarray);
 
 			System.out.println("Do you want to play again?");
 			System.out.println();
@@ -48,45 +111,48 @@ public class NumberSpel {
 				again = false; // stoppar loopen som kör om spelet
 				System.out.println("Okay, Goodbye");
 				System.out.println();
-				
-				for(int i = 0; i < intarray.length; i++) { // skriver ut arrayen
+
+				for (int i = 0; i < intarray.length; i++) { // skriver ut arrayen som en poång lista
 					System.out.println(intarray[i]);
 				}
-				
+
 			}
 		}
 		sc.close();
 	}
-	
+
 	static void bubbleSort(int[] intarray) {
 
-        // initierar nödvändiga variabler
-        boolean swapped = true;
-        int counter = 0;
+		// initierar nödvändiga variabler
+		boolean swapped = true;
+		int counter = 0;
 
-        // loopen körs tills hela arrayen kollas igenom utan att några tal byter plats med varandra
-        do {
+		// loopen körs tills hela arrayen kollas igenom utan att några tal byter plats
+		// med varandra
+		do {
 
-            swapped = false;
+			swapped = false;
 
-            //loopar igenom hela arrayen 1 gång
-            for (int i = 0; i < intarray.length - 1; i++) {
+			// loopar igenom hela arrayen 1 gång
+			for (int i = 0; i < intarray.length - 1; i++) {
 
-                // jämför två tal bredvid varandra och byter plats på dem om det högra talet är större än det vänstra
-                if (intarray[i] > intarray[i + 1]) {
-                    int temp = intarray[i + 1];
-                    intarray[i + 1] = intarray[i];
-                    intarray[i] = temp;
-                    swapped = true; // har åtminstone ett byte skett under körningen så ändras swapped värde till true
-                }
-                // håller koll på antalet iterationer
-                counter++;
-            }
+				// jämför två tal bredvid varandra och byter plats på dem om det högra talet är
+				// större än det vänstra
+				if (intarray[i] > intarray[i + 1]) {
+					int temp = intarray[i + 1];
+					intarray[i + 1] = intarray[i];
+					intarray[i] = temp;
+					swapped = true; // har åtminstone ett byte skett under körningen så ändras swapped värde till
+									// true
+				}
+				// håller koll på antalet iterationer
+				counter++;
+			}
 
-        } while (swapped); // do-while loopen körs alltid minst en gång
+		} while (swapped); // do-while loopen körs alltid minst en gång
 
-    }
-	
+	}
+
 	public static int Spel(int tal) {
 		Scanner scanner = new Scanner(System.in); // skapar en ny scanner
 		Boolean spel = true;

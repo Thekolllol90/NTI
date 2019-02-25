94ec3985e4c095dd9c994367368a88448f18ccc3
Thekolllol90 <neosvarre@hotmail.se>
2019-02-25 10:33:37 +0100

    börjat på en poäng lista

--- src/V8/NumberSpel.java
+++ src/V8/NumberSpel.java
@@ -9,13 +9,14 @@ public class NumberSpel {
 		boolean again = true;
 		Scanner sc = new Scanner(System.in);
 		Random random = new Random(); // genererar ett random tal
-		// String dif = "0";
+		int intarray[] = {		// skaapr en array som poäng lista
+				99, 88, 33, 44, 55
+		};
 		while (again == true) {
 
 			System.out.println("choose a difficulty");
 			System.out.println();
 			System.out.println("1.easy   2.medium   3.hard  4.imposible");
-			// dif = sc.nextLine();
 			int dif = sc.nextInt();
 
 			int tal = 0;
@@ -30,7 +31,12 @@ public class NumberSpel {
 			}
 			System.out.println("Im thinking of a number betwen 0 and " + (val - 1));
 			tal = random.nextInt(val);
-			Spel(tal); // hämtar methoden
+			int rep = Spel(tal); // hämtar methoden
+			
+			
+			intarray[intarray.length - 1]
+			
+			bubbleSort(intarray);
 
 			System.out.println("Do you want to play again?");
 			System.out.println();
@@ -41,11 +47,46 @@ public class NumberSpel {
 			} else if (ag.toLowerCase().equals("n")) { // gör alla siffror till lowercase och kollar om man skriver N
 				again = false; // stoppar loopen som kör om spelet
 				System.out.println("Okay, Goodbye");
+				System.out.println();
+				
+				for(int i = 0; i < intarray.length; i++) { // skriver ut arrayen
+					System.out.println(intarray[i]);
+				}
+				
 			}
 		}
 		sc.close();
 	}
+	
+	static void bubbleSort(int[] intarray) {
 
+        // initierar nödvändiga variabler
+        boolean swapped = true;
+        int counter = 0;
+
+        // loopen körs tills hela arrayen kollas igenom utan att några tal byter plats med varandra
+        do {
+
+            swapped = false;
+
+            //loopar igenom hela arrayen 1 gång
+            for (int i = 0; i < intarray.length - 1; i++) {
+
+                // jämför två tal bredvid varandra och byter plats på dem om det högra talet är större än det vänstra
+                if (intarray[i] > intarray[i + 1]) {
+                    int temp = intarray[i + 1];
+                    intarray[i + 1] = intarray[i];
+                    intarray[i] = temp;
+                    swapped = true; // har åtminstone ett byte skett under körningen så ändras swapped värde till true
+                }
+                // håller koll på antalet iterationer
+                counter++;
+            }
+
+        } while (swapped); // do-while loopen körs alltid minst en gång
+
+    }
+	
 	public static int Spel(int tal) {
 		Scanner scanner = new Scanner(System.in); // skapar en ny scanner
 		Boolean spel = true;
@@ -69,6 +110,6 @@ public class NumberSpel {
 			}
 		}
 
-		return 0;
+		return y;
 	}
 }

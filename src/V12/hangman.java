c98e8745777c7b57f3c174829efb2c568eab8721
Thekolllol90 <neosvarre@hotmail.se>
2019-03-20 19:29:27 +0100

    börjat lite på hangman

--- src/V12/hangman.java
+++ src/V12/hangman.java
@@ -1,5 +1,22 @@
 package V12;
 
-public class hangman {
+import java.util.Scanner; 
 
+public class hangman {
+	public static void main(String[] args) {
+		
+		boolean play = true;
+		
+		while(play) {
+			
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("What is the word?");
+			String word = scanner.nextLine();
+			
+			String wordLower;
+			wordLower = word.toLowerCase();
+			
+			
+		}
+	}
 }
